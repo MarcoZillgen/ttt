300d3f6fac155355a60fd3dd3c5dd15247c034bb
Marco Simon Zillgen <marcosimonzillgen@gmail.com>
2025-07-30 22:42:07 +0200

    begin

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,173 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useEffect, useState } from "react";
+
+type Player = "X" | "O";
+
+type Cell = Player | null;
+
+type Board = [Cell, Cell, Cell, Cell, Cell, Cell, Cell, Cell, Cell];
+
+type GameState = [
+  Board,
+  Board,
+  Board,
+  Board,
+  Board,
+  Board,
+  Board,
+  Board,
+  Board
+];
+
+const colors = {
+  X: "#84BC9C",
+  O: "#F46197",
+  background: "#FFFDF780",
+};
+
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+function XWonBoard(board: Board): boolean {
+  return winningCombinations.some((combination) =>
+    combination.every((index) => board[index] === "X")
+  );
+}
+
+function OWonBoard(board: Board): boolean {
+  return winningCombinations.some((combination) =>
+    combination.every((index) => board[index] === "O")
+  );
+}
+
+function isWonBoard(board: Board): boolean {
+  return winningCombinations.some(
+    (combination) =>
+      combination.every((index) => board[index] === "X") ||
+      combination.every((index) => board[index] === "O")
+  );
+}
+
+function isBoardFull(board: Board): boolean {
+  return board.every((cell) => cell !== null);
+}
+
+function unableToFocus(board: Board): boolean {
+  return isWonBoard(board) || isBoardFull(board);
+}
+
+function gameIsWon(gameState: GameState): boolean {
+  return winningCombinations.some(
+    (combination) =>
+      combination.every((index) => OWonBoard(gameState[index])) ||
+      combination.every((index) => XWonBoard(gameState[index]))
+  );
+}
+
+function winner(gameState: GameState): Player | null {
+  for (const combination of winningCombinations) {
+    if (combination.every((index) => XWonBoard(gameState[index]))) {
+      return "X";
+    }
+    if (combination.every((index) => OWonBoard(gameState[index]))) {
+      return "O";
+    }
+  }
+  return null;
+}
+
+export default function Page() {
+  const [gameState, setGameState] = useState<GameState>();
+  const [playerTurn, setPlayerTurn] = useState<Player>("X");
+  const [focusedCell, setFocusedCell] = useState<number | null>(null);
+
+  useEffect(() => {
+    const initialBoard: GameState = Array(9)
+      .fill(null)
+      .map(() => Array(9).fill(null) as Board) as GameState;
+    setGameState(initialBoard);
+  }, []);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div
+      className="flex w-screen h-screen items-center justify-center"
+      style={{ backgroundColor: colors[playerTurn] }}
+    >
+      <div
+        className="grid grid-cols-3 gap-4 aspect-square landscape:h-full p-4 portrait:w-full"
+        style={{ backgroundColor: colors.background }}
+      >
+        {gameState && gameIsWon(gameState) ? (
+          <div
+            className="col-span-3 text-center text-2xl font-bold rounded grid place-items-center"
+            style={{ backgroundColor: colors[winner(gameState)!] }}
+          >WINNER</div>
+        ) : (
+          gameState?.map((board, index) => (
+            <div
+              key={index}
+              className="grid grid-cols-3 gap-3 aspect-square rounded-lg overflow-hidden p-2"
+              style={{
+                background:
+                  focusedCell === index
+                    ? colors[playerTurn] + "80"
+                    : "transparent",
+              }}
+            >
+              {XWonBoard(board) ? (
+                <div
+                  className="border size-full aspect-square rounded-lg flex items-center justify-center col-span-3"
+                  style={{ backgroundColor: colors.X }}
+                ></div>
+              ) : OWonBoard(board) ? (
+                <div
+                  className="border size-full aspect-square rounded-lg flex items-center justify-center col-span-3"
+                  style={{ backgroundColor: colors.O }}
+                ></div>
+              ) : (
+                board.map((cell, cellIndex) => (
+                  <div
+                    key={cellIndex}
+                    className="border flex items-center justify-center size-full aspect-square rounded-lg"
+                    style={{
+                      backgroundColor:
+                        cell === "X"
+                          ? colors.X
+                          : cell === "O"
+                          ? colors.O
+                          : colors.background,
+                    }}
+                    onClick={() => {
+                      if (cell || !gameState) return;
+                      if (focusedCell !== null && focusedCell !== index) {
+                        return;
+                      }
+                      const newGameState = [...gameState];
+                      newGameState[index][cellIndex] = playerTurn;
+                      setGameState(newGameState as GameState);
+                      setPlayerTurn(playerTurn === "X" ? "O" : "X");
+                      if (unableToFocus(newGameState[cellIndex])) {
+                        setFocusedCell(null);
+                      } else {
+                        setFocusedCell(cellIndex);
+                      }
+                    }}
+                  ></div>
+                ))
+              )}
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
 }
