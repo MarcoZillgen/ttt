cab99e0fad7347e9b703dd61855a9acc7e5d447c
Marco Simon Zillgen <marcosimonzillgen@gmail.com>
2025-12-17 07:05:44 +0100

    nightly, its too late

--- app/component/SuperBoardGrid.tsx
+++ app/component/SuperBoardGrid.tsx
@@ -7,7 +7,7 @@ export function SuperBoardGrid() {
   const { game, dispatch } = useGame();
 
   return (
-    <div className="size-[80vmin] grid gap-4 grid-cols-3 grid-rows-3">
+    <div className="md:size-[80vmin] size-[90vmin] grid gap-4 grid-cols-3 grid-rows-3">
       {game.superBoard.map((subBoard, subBoardIndex) => (
         <SubBoardGrid
           key={subBoardIndex}
